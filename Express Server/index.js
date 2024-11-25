2a586cc85ede17bda9168841dd0811973ef22b9f
Paul <mwapaul265@gmail.com>
2024-11-25 11:59:18 +0300

    Server sety up

---
+++ Express Server/index.js
@@ -0,0 +1,11 @@
+ import { log } from "console";
+import express from "express"
+ const app = express();
+ var port = 3000;
+
+
+ app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+    
+ })
+
---
+++ using-node/index.js
@@ -0,0 +1,5 @@
+console.log("Hello lets write node");
+var name = 'Paul';
+if (name === 'Paul') {
+    console.log("The name is " + name)
+}
