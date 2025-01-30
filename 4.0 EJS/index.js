33052bd7f0ae72bf8b7e842d57c3de4e336c75b2
Paul <mwapaul265@gmail.com>
2025-01-30 04:57:20 +0300

    passing data from template

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res)=>{
     let day = today.getDay()
     const type = "Its a week day"
     const adv = "You need to grind harder";
+    const  data ={
+        title: "Home Page",
+        seconds: new Date().getSeconds(),
+        items : ["Apple", "Banana", "Orange", "Mango"],
+        htmlContent : "<h2> This is a heading </h2>",
+    }
 
     if (day === 6 || day === 0){
         const type = "Its a weekend";
@@ -15,7 +21,14 @@ app.get('/', (req, res)=>{
     }
     res.render("index.ejs",{
         dayType : type,
-        advice : adv
+        advice : adv,
+        data : data,
+    })
+})
+app.get('/fun', (req, res)=>{
+    let funGames = ["Chess", "Ludo", "PUBG", "COD", "Among Us", "Fortnite"];
+    res.render("fun.ejs", {
+        funGames : funGames,
     })
 })
 
---
+++ 4.2 Passing Data/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+
+  res.render(
+    'index.ejs'
+  )
+});
+
+app.post("/submit", (req, res) => {
+  let fName = req.body['fName'];
+  let lName = req.body['lName'];
+  let wordCount = fName.length + lName.length;
+  console.log(wordCount);
+  app.render('index.ejs',{wordCount})
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
